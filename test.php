31430772e8cf307808cfed4d52867a27ce192b66
CallmeGorgus <62182885+CallmeGorgus@users.noreply.github.com>
2020-03-14 17:56:32 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,5 @@
+
+<?php
+phpinfo(); // vypíše všechny informace
+phpinfo(INFO_LICENSE); // vypíše informace o licenci
+phpinfo(INFO_LICENSE | INFO_CREDITS); // vypíše informace o licenci a zásluhách
