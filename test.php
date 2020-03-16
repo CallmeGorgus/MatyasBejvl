56d1008986ff4c3323081a895d0d97ba084cf4ce
Matyáš Bejvl <62182885+CallmeGorgus@users.noreply.github.com>
2020-03-16 16:00:47 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,362 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Save the world- aktualne</title>
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="css/screen.css">
+    </head>
+    <body>  
+        <img src="images/il be tracer.png" width="350" height="350 ">
+        <a href="http://www.seznam.cz" target="_blank"><img src="https://cdn.drawception.com/drawings/wJpz00o3yD.png"></a>
+        <br>
+        <input type="submit" value="odeslat"> 
+        <table border="1">
+            <tr>
+                <td width="100">40 VB </td>
+                <td width="30" align="center">Ceny</td>
+                <td width="20">čas</td>
+            </tr>
+            <tr>
+                <td width="20">Perks</td>
+                <td width="200"> zatim nic</td>
+                <td width="20">Nic</td>
+            </tr>
+        </table>
+        <form action="index.html" method="get">
+            něco
+            ...<input type=submit value=zkontrolovatt>
+            <select name="predmety">
+                <option value="0">VB</option>
+                <option value="0">VB1</option>
+                <option value="0">VB2</option>
+                <option value="0">VB3</option>
+                <option value="0">VB4</option>
+            </select>
+            <br>
+            <input type="file" name="procházet"> 
+            <br>
+            <input type="checkbox" name="dobrý" >
+            <input type="checkbox" name="dobrý" >   
+            <input type="checkbox" name="špatný">
+            <input type="checkbox" name="jde to">
+            <br>
+            <input type="radio" name="hodnota" value="0">Vubec
+            <input type="radio" name="hodnota" value="0">velmi
+            <br>
+            <textarea rows="4" cols="40" class="center" name="nic">Napiš, jak se ti libí naše Timed mission</textarea>
+            <br>
+            <button name="klikatko" value="klikatko">tlačítko s <b>černy</b></button>
+        </form>
+
+        <p style="text-align:center "></p>
+
+        <h1> Timed missions </h1> 
+        <h6> if you dont want be on this page, go here ↓ </h6>
+        <a href="https://freethevbucks.com/timed-missions/"> odkaz na jinou stranku</a>
+        <ol>
+            <li> Jde o odmeny, ktere se resetuji každých 6h v Save the wordu</li>
+            <li> je šance na VB </li>
+        </ol>
+        <br>     
+        <br>
+        <a href="//localhost/html/html1.html">relevantní</a>
+        <br>
+
+
+
+
+
+
+
+    </body>
+
+
+</html>
+
+
+
+
+<?PHP
+echo ("Ahoj");
+
+print_r("Ahoj");
+
+var_dump("Ahoj");
+
+echo "\$neco";
+
+echo "<a href =\"index.php\">Homepage</a>";
+
+$a = 2;
+$b = 3;
+$c = '3';
+
+var_dump(var_dump($a === $b));
+
+var_dump($b === $c);
+var_dump($a == $b);
+var_dump($b == $c);
+var_dump($a && $b);
+var_dump($a++);
+var_dump($a != $b);
+var_dump($a > $b);
+var_dump($a || $b);
+var_dump(!$a);
+
+
+
+
+
+
+
+$cena1 = 12;
+$cena2 = 13;
+
+
+
+if ($cena1 > $cena2) {
+    echo "je vetsi";
+} else {
+    echo "je mensi";
+}
+
+echo "<br>";
+echo "<br>";
+
+
+if ($cena1 != $cena2) {
+    echo "Nerovna se.";
+}
+
+$bila = 2;
+$cerna = 1;
+
+if ($bila > $cerna) {
+    echo "belejsi";
+} else {
+    echo "tmavejsi";
+}
+
+$bila > 20;
+echo "zadna barva";
+if ($bila < 20) {
+    echo "barevny";
+} else {
+    
+} echo "nic";
+
+$neco1 = 1;
+$neco2 = 2;
+
+$neco = $neco1 != $neco2 ? $neco1 : $neco2;
+
+
+if ($neco1 > $neco2) {
+    echo "neco";
+} elseif ($neco1 < $neco2) {
+    echo "necos";
+}
+
+
+
+
+
+$zelenina = array('kedlubna', 'rajce', 'paprika', 'meloun', 'cuketa', 'lilek', 'cibule', 'cesnek', 'mrkev', 'hrasek');
+$zelenina = array(10 => 'kedlubna', 20 => 'rajce', 30 => 'paprika', 40 => 'meloun', 50 => 'cuketa', 60 => 'lilek', 70 => 'cibule', 80 => 'cesnek', 90 => 'mrkvev', 100 => 'hrasek');
+$zelenina [21] = 'steblo';
+var_dump($zelenina);
+
+
+
+$auta ['osobni'] ['skoda'] = 'Fabia';
+$auta ['osbni'] ['skoda'] [] = 'octavia';
+$auta ['osbni'] ['skoda'] [] = 'rxt';
+$auta ['osbni'] ['skoda'] [] = 'bnw';
+$auta ['osbni'] ['skoda'] [] = 'carbon';
+$auta ['osbni'] ['skoda'] [] = 'ferrari';
+$auta ['osbni'] ['skoda'] [] = 'nune';
+$auta ['osbni'] ['skoda'] [] = 'borsavia';
+$auta ['osbni'] ['skoda'] [] = 'tesla';
+$auta ['osbni'] ['skoda'] [] = 'hrabak';
+$auta ['osobni'] ['skoda'] = 'Fabia1';
+$auta ['osbni'] ['skoda'] [] = 'octavia2';
+$auta ['osbni'] ['skoda'] [] = 'rxt3';
+$auta ['osbni'] ['skoda'] [] = 'bnw4';
+$auta ['osbni'] ['skoda'] [] = 'carbon5';
+$auta ['osbni'] ['skoda'] [] = 'ferrari6';
+$auta ['osbni'] ['skoda'] [] = 'nune7';
+$auta ['osbni'] ['skoda'] [] = 'borsavia8';
+$auta ['osbni'] ['skoda'] [] = 'tesla9';
+$auta ['osbni'] ['skoda'] [] = 'hrabak10';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var_dump($auta);
+
+
+
+
+/*
+  $auta ['nakladni'] ['skoda'] = 'nakladak1';
+  $auta ['nakladni'] ['skoda'] = 'nakladak2';
+  $auta ['nakladni'] ['skoda'] = 'nakladak3';
+  $auta ['nakladni'] ['skoda'] = 'nakladak7';
+  $auta ['nakladni'] ['skoda'] = 'nakladak8';
+  $auta ['nakladni'] ['skoda'] = 'nakladak4';
+  $auta ['nakladni'] ['skoda'] = 'nakladak5';
+  $auta ['nakladni'] ['skoda'] = 'nakladak6';
+  $auta ['nakladni'] ['skoda'] = 'nakladak9';
+  $auta ['nakladni'] ['skoda'] = 'nakladak10';
+
+ */
+echo $zelenina [20];
+echo"<table border='1'>";
+foreach ($zelenina as $key => $value) {
+
+    echo "<tr>"
+    . "<td>" . $key . "</td>"
+    . "<td>" . $value . "</td>"
+    . "</tr>";
+}
+echo "</table>";
+
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+
+$count = 20;
+$count2 = 5;
+
+echo"<table border='1'>";
+
+for ($index = 1; $index < $count + 1; $index++) {
+
+    echo "<tr>";
+
+
+
+    for ($index2 = 1; $index2 < $count2 + 1; $index2++) {
+
+        echo "<td>" . $index . "-" . $index2 . "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+
+
+
+
+
+/*
+function isValid($param1,$param2) {
+foreach (param1 as $key => $value) {
+    if (strpos ($param2, $value)) {
+        echo true;
+    }
+    }
+    echo false;
+}
+   
+            // 1.
+           var_dump($a++); //anotace v kodu
+                
+   //2. 
+           $proměnna1 = 4;
+           echo ($proměnná1);
+           //3
+           echo     '$proměnna' - $proměnná1
+      //4
+          // echo '<img src="OMG.JPG"alt="babka" />';
+           //5
+           //DEFINE("PI", 3.14); 
+           //6
+          // $data="yes";
+           //7
+          // $data=true;
+           //8
+           //$a+$b , $a-$b , $a/$b
+                   //9
+               //    $a==$b , $a!=$b
+                   //10
+                   //k $a se přičte 1 
+           //11
+           //přiradí hodnotu proměnné
+           //12
+           //NEROVNOST, vypíše TRUEpokud se výsledek opravdu nerovná
+           //13
+           //Určení toho, jak se bude skript dále vyvíjet 
+           //14
+        //   if($a==$b){
+         //      echo ("rovnají se");
+         //  }
+           //15
+         //  while 
+               //16
+               //uživatelem definovaná funkce
+               //interní funkce
+               //anonymní funkce
+           
+           
+############################################################
+
+    ?>
+    <?php
+   
+class eldesGlobal implements  {
+               
+     
+
+
+
+
+}
+
+class ustredny {
+    
+    
+    
+}
+
+class ustredna implements Izdroj12vdc {
+    
+
+    
+    
+  */ 
+
+
+?>
+
 
 <?php
 phpinfo(); // vypíše všechny informace
