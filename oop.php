f1b540d191a9ae994fc97c07e30338d017806191
Matyáš Bejvl <62182885+CallmeGorgus@users.noreply.github.com>
2020-03-30 19:44:14 +0200

    Create oop.php

---
+++ oop.php
@@ -0,0 +1,29 @@
+<?php
+
+class A {
+
+publicfunction_construct($ustrednyPrefix.php) {
+}
+
+}
+class B {
+
+publicfunction _construct ($ustrednaPrefix.php) {
+}
+
+}
+
+class ustrednaPrefix extends ustrednyPrefix {}
+
+$napeti['ustrednaprefix'];
+ustrednaPrefix['setNapetiPrefix']="cislo";
+
+include('ustrednyPrefix.php');
+
+class ustrednyPrefix {Bej};
+
+$napetiPrefix['firstname']=Matyas;
+
+$this=>$napeti = ustrednaPrefix;
+
+var_dump(ustrednaPrefix);
