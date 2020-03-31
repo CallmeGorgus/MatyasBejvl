c434af56bf5e8ebf701c86af1ec8706587f9aa89
Matyáš Bejvl <62182885+CallmeGorgus@users.noreply.github.com>
2020-03-31 15:55:18 +0200

    Update oop.php

--- oop.php
+++ oop.php
@@ -33,3 +33,45 @@ $this=>$napeti = ustrednaPrefix;
 
 var_dump(ustrednaPrefix);
 ?>
+
+//////////////////////////////////////////////////////////////////////////////////
+
+public function translate ($text){
+$translate text = $this->ts->translate($text);
+
+return $translate_text;
+}
+
+public function translate(string $text){
+$translate_text = $this->ts->translate($text);
+
+return $translate_text;
+}
+
+public function translate(string $text) : string {
+$translate_text = $this->ts->translate($text);
+return $translate text;
+}
+
+/**
+* @param string $text
+* @return string
+*/
+public function translate (string $text) : string{
+$translate_text = $this->ts->translate ($text);
+return $translate_text;
+}
+
+/** Vrati prelozeny text
+* @author Matyaas Bej
+* @param string $text
+* @return string
+*/
+public function translate (string $text) : string {
+// preklada text
+$translate text = $this->ts->translate($text);
+/**
+* nebo viceradkovy
+* komentar
+*/
+return $translate_text;
