07a7194238ef7ad89b73bac47a3bf68a601ef524
Matyáš Bejvl <62182885+CallmeGorgus@users.noreply.github.com>
2020-05-25 22:09:41 +0200

    Numbers

--- INDEX.php
+++ INDEX.php
@@ -1,7 +1,28 @@
 <?php
+class numbersBejvlMatyas {
 
-require 'vendor/autoload.php'
-use Tracy\Debugger;
+    /**
+     *  zaokrouhlý cislo podle presnosti
+     * @param float $num
+     * @param int $pr
+     * @return float
+     */
+    public static function round(float $num, int $pr = 0): float {
+        return round($num, $pr);
+    }
 
-Debugger::enable();
-?>
+    /**
+     * vynasobii
+     * @param float $a
+     * @param float $b
+     * @return float
+     */
+    public static function vynasob(float $a, float $b): float {
+        return $a * $b;
+    }
+
+    public static function abs(float $number): float {
+        return self :: abs($number);
+    }
+
+}   
